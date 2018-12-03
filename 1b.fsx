395fa4c1f9cf1c1f6eb70b569886c4c95c11f735
Ryan Clarke <ryan@ryanclarke.net>
2018-12-03 13:38:26 -0500

    Improve performance of 1b

--- 1b.fsx
+++ 1b.fsx
@@ -1,23 +1,14 @@
 let freqs = System.IO.File.ReadLines("input/1.txt") |> Seq.map int |> Seq.toArray
-let freqLength = freqs |> Array.length
-let getFreq index =
-    freqs.[index % freqLength]
-    
+let nextFreq i = freqs.[i % freqs.Length]
+
 let lookForDuplicate =
-    Seq.unfold (fun state ->
-        let found, index, list = state
-        if (found) then
+    let finder state =
+        let found, sum, index, (set:Set<int>) = state
+        if found then
             None
-        else 
-            let freq = freqs.[index % (freqs |> Array.length)]
-            let newList = 
-                match list with
-                | [] -> [ freq ]
-                | x :: _ -> freq + x :: list
-            
-            if (list |> List.contains (newList |> List.head)) then
-                Some ((true, index+1, newList), (true, index+1, newList))
-            else
-                Some (state, (found, index+1, newList))) (false, 0, [0])
+        else
+            let newSum = sum + nextFreq index
+            Some (newSum, (set.Contains newSum, newSum, index+1, set.Add newSum))
+    Seq.unfold finder (false, 0, 0, Set.empty)
 
 lookForDuplicate |> Seq.last |> printfn "%A"

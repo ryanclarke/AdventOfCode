033d56b6a90cfcd9267208a0cc2031fc1448c577
Ryan Clarke <ryan@ryanclarke.net>
2018-12-04 05:20:23 -0500

    Optimize 3a with a folded set

--- 3a.fsx
+++ 3a.fsx
@@ -1,41 +1,31 @@
 open System.Drawing
 open System
 
-type Claim = {
-    Id: int
-    Rect: Rectangle
-    }
-
-let mutable chart = Array.create 1000000 false;
+type Claim = { Id: int; Rect: Rectangle }
 
 let parseClaim (s:string) =
     s.Split([|' '; '#'; '@'; ','; 'x'; ':'|])
-        |> Array.filter (String.IsNullOrWhiteSpace >> not)
-        |> Array.map int
-        |> fun x ->
-            {
-                Id = x.[0]
-                Rect = new Rectangle(x.[1], x.[2], x.[3], x.[4])
-            }
+    |> Array.filter (String.IsNullOrWhiteSpace >> not)
+    |> Array.map int
+    |> fun x -> { Id = x.[0]; Rect = new Rectangle(x.[1], x.[2], x.[3], x.[4]) }
 
-let intersects a b =
+let intersects a (set:Set<int*int>) b =
     if (a.Rect.IntersectsWith(b.Rect)) then
-        let inter = Rectangle.Intersect (a.Rect, b.Rect)
-        seq { inter.Top .. inter.Bottom-1 }
-            |> Seq.iter (fun x ->
-                seq { inter.Left .. inter.Right-1 }
-                    |> Seq.iter (fun y ->
-                        chart.[x*1000 + y] <- true))
-        Some (inter)
+        let rect = Rectangle.Intersect (a.Rect, b.Rect)
+        seq { rect.Top .. rect.Bottom-1 }
+        |> Seq.fold (fun (set:Set<int*int>) x ->
+            seq { rect.Left .. rect.Right-1 }
+            |> Seq.fold (fun (set:Set<int*int>) y ->
+                set.Add (x, y)) set) set
     else
-        None
+        set
 
-let claims =
-    System.IO.File.ReadLines("input/3.txt")
-        |> Seq.map parseClaim
-        |> Seq.toList
-
-claims.[..(claims.Length-2)]
-    |> List.mapi (fun i x -> claims.[i+1..] |> List.map (intersects x) |> List.choose id) 
-
-chart |> Array.filter id |> Array.length |> printfn "%A"
+System.IO.File.ReadLines("input/3.txt")
+|> Seq.map parseClaim
+|> Seq.toList
+|> fun claims ->
+    claims.[..(claims.Length-2)]
+    |> List.mapi (fun i c -> claims.[i+1..] |> List.fold (intersects c) Set.empty)
+    |> List.reduce Set.union
+    |> Set.count
+    |> printfn "%A"

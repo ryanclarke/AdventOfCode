8ebb1a1896860488c2a89ff78892bbe9a0bf00cd
Ryan Clarke <ryan@ryanclarke.net>
2018-12-05 06:01:32 -0500

    Refactor 4 to be nicer and prettier and shorter

--- 4.fsx
+++ 4.fsx
@@ -2,26 +2,12 @@ open System
 open System.Text.RegularExpressions
 
 let parse pattern input =
-    let regex = sprintf "^\[(?<datetime>[0-9 :-]+)\] (?<act>%s)$" pattern
-    let m = Regex.Match(input, regex)
-    if (m.Success) then Some (m.Groups) else None
+    let m = Regex.Match(input, pattern)
+    if (m.Success) then Some (m.Groups.[1].Value |> int) else None
 
-let (|BeginShiftLog|_|) log = parse "Guard #(?<guard>\d+) begins shift" log
-let (|AsleepLog|_|) log = parse "falls asleep" log
-let (|AwakeLog|_|) log = parse "wakes up" log
-
-type Msg =
-    | BeginShift of int
-    | Asleep of DateTime
-    | Awake of DateTime
-    | FAIL
-
-let toMsg log =
-    match log with
-    | BeginShiftLog x -> BeginShift (int x.["guard"].Value)
-    | AsleepLog x -> Asleep (DateTime.Parse(x.["datetime"].Value))
-    | AwakeLog x -> Awake (DateTime.Parse(x.["datetime"].Value))
-    | _ -> FAIL
+let (|BeginShift|_|) = parse "Guard #(\d+)"
+let (|Asleep|_|) = parse ":(\d{2})\] falls asleep"
+let (|Awake|_|) = parse ":(\d{2})\] wakes up"
 
 type State = {
     CurrentGuard: int
@@ -30,46 +16,48 @@ type State = {
 }
 let emptyState = {CurrentGuard = 0; LastAsleep = 0; GuardMinutes = Map.empty}
 
-let minutesSleptRecorder (state:State) (msg:Msg) =
-    match msg with
-    | BeginShift i ->
+let minutesSleptRecorder (state:State) (log:string) : State =
+    match log with
+    | BeginShift guardId ->
         { state with 
-            CurrentGuard = i; 
+            CurrentGuard = guardId; 
             GuardMinutes =
-                if (state.GuardMinutes |> Map.containsKey i) then
-                    state.GuardMinutes
-                else
-                    state.GuardMinutes.Add (i, Array.zeroCreate(59)) }
-    | Asleep dt -> 
+                match state.GuardMinutes.TryFind guardId with
+                | Some _ -> state.GuardMinutes
+                | None -> state.GuardMinutes.Add (guardId, Array.zeroCreate(59)) }
+    | Asleep asleepMinute -> 
         { state with
-            LastAsleep = dt.Minute }
-    | Awake dt ->
-        let tup =
-            let mins = seq { state.LastAsleep .. dt.Minute-1 }
+            LastAsleep = asleepMinute }
+    | Awake awakeMinute ->
+        let minutes =
+            let napMins = seq { state.LastAsleep .. awakeMinute-1 }
             state.GuardMinutes.[state.CurrentGuard]
-            |> Array.mapi (fun i x ->
-                if (mins |> Seq.contains i) then
-                    x + 1
-                else
-                    x)
+            |> Array.mapi (fun i x -> if (napMins |> Seq.contains i) then x + 1 else x)
         { state with
-            GuardMinutes = 
-                state.GuardMinutes.Add (state.CurrentGuard, tup) }
-    | FAIL -> 
+            GuardMinutes = state.GuardMinutes |> Map.add state.CurrentGuard minutes }
+    | _ -> 
         state
+
+type GuardMinutes = {
+    Id: int
+    TimesSleptByMinute: int[]
+} with
+    member this.SleepiestMinute =
+        Array.IndexOf(this.TimesSleptByMinute, (Array.max this.TimesSleptByMinute))
 
 let guardMinutes =
     System.IO.File.ReadLines("input/4.txt")
     |> Seq.sort
-    |> Seq.map toMsg
     |> Seq.fold minutesSleptRecorder emptyState
-    |> fun x -> x.GuardMinutes
+    |> fun x -> x.GuardMinutes 
+    |> Map.toSeq
+    |> Seq.map (fun x -> {Id = fst x; TimesSleptByMinute = snd x})
 
 let strategy number func =
     guardMinutes
-    |> Seq.maxBy (fun x -> x.Value |> func)
-    |> fun x -> (x.Key * (Array.IndexOf(x.Value, (Array.max x.Value))))
-    |> printfn "Strategy %d : %A" number
+    |> Seq.maxBy (fun x -> x.TimesSleptByMinute |> func)
+    |> fun x -> x.Id * x.SleepiestMinute
+    |> printfn "Strategy %d: %A" number
 
 strategy 1 Array.sum
 strategy 2 Array.max

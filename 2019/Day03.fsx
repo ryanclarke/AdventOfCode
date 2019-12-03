9c56e8b886a8483b7fe85a1e053e67334b0cdad2
Ryan Clarke <ryan@ryanclarke.net>
2019-12-03 14:16:44 -0500

    2019.03b Solved

--- 2019/Day03.fsx
+++ 2019/Day03.fsx
@@ -5,53 +5,68 @@ let input =
     inputFile "03" string
     |> Seq.toArray
 
-type Segment =
-    | H of (int*int*int)
-    | V of (int*int*int)
+type Line = {
+    track: int
+    start: int
+    max: int
+    min: int
+    steps: int
+}
 
-let vectorize (x,y) (vector:string) =
+type Segment =
+    | H of Line
+    | V of Line
+
+let segmentize (x,y,steps) (vector:string) =
     let direction = vector.[0]
     let distance = int vector.[1..]
+    let newSteps = steps + distance
     match direction with
-    | 'U' -> (H (x,y + distance,y),(x, y + distance))
-    | 'D' -> (H (x,y,y - distance),(x, y - distance))
-    | 'R' -> (V (y,x + distance,x),(x + distance, y))
-    | 'L' -> (V (y,x,x - distance),(x - distance, y))
+    | 'U' -> (H {track = x; start = y; max = y + distance; min = y; steps = steps},(x, y + distance, newSteps))
+    | 'D' -> (H {track = x; start = y; max = y; min = y - distance; steps = steps},(x, y - distance, newSteps))
+    | 'R' -> (V {track = y; start = x; max = x + distance; min = x; steps = steps},(x + distance, y, newSteps))
+    | 'L' -> (V {track = y; start = x; max = x; min = x - distance; steps = steps},(x - distance, y, newSteps))
     | _ -> failwith "Invalid direction"
 
 let toSegments str =
     str
     |> ssplit [|','|]
-    |> Array.mapFold vectorize (0,0)
+    |> Array.mapFold segmentize (0,0,0)
     |> fst
     |> Array.toSeq
 
-let crossing (segment1:Segment) (segment2:Segment) =
+let between a b =
+    a.max > b.track && b.track > a.min
+
+let intersection (segment1:Segment) (segment2:Segment) =
     match segment1, segment2 with
-    | H h, V v | V v, H h ->
-        match h,v with
-        | (x,ymax,ymin),(y,xmax,xmin) when xmax >= x && x >= xmin && ymax >= y && y >= ymin -> Some (abs x + abs y)
-        | _ -> None
+    | H h, V v | V v, H h
+        when between v h && between h v ->
+            let distance = h.steps + v.steps + (abs (v.track - h.start)) + (abs (h.track - v.start))
+            Some (h.track,v.track,distance)
     | _ -> None
 
-let closest (wire2:seq<Segment>) (segment:Segment) =
-    let crosses =
-        wire2
-        |> Seq.choose (crossing segment)
-        |> Seq.where (fun x -> x > 0)
-        |> Seq.toList
+let intersectionsWith (wire:seq<Segment>) (segment:Segment) =
+    wire
+    |> Seq.choose (intersection segment)
+    |> Seq.where (fun (x,y,_) -> x <> 0 && y <> 0)
+    |> Seq.toList
 
-    match crosses with
-    | [] -> None
-    | x -> List.min x |> Some
-
-let solve (input:string[]) =
+let solve toDistance (input:string[]) =
     let wire1,wire2 =
         Array.map toSegments input
         |> fun wires -> wires.[0],wires.[1]
     wire1
-    |> Seq.choose (closest wire2)
+    |> Seq.collect (intersectionsWith wire2)
+    |> Seq.map toDistance
     |> Seq.min
 
-solve input
-|> solution "03a"
+let manhattanDistance (x,y,_) = abs x + abs y
+
+input
+|> solve manhattanDistance |> solution "03a 1211"
+
+let combinedSteps (_,_,s) = s
+
+input
+|> solve combinedSteps |> solution "03b 101386"

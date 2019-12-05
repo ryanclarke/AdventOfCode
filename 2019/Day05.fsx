17ce7afa5a48db041173d503e73411d8dcc09422
Ryan Clarke <ryan@ryanclarke.net>
2019-12-05 09:01:34 -0500

    Improve Run script

--- 2019/Day05.fsx
+++ 2019/Day05.fsx
@@ -131,40 +131,42 @@ intcodeComputer.run [1] program
 intcodeComputer.run [5] program
 |> List.head |> solution "05b" 12648139
 
-intcodeComputer.run [7] [|3;9;8;9;10;9;4;9;99;-1;8|]
-|> List.head |> solution "05b.1a" 0
-intcodeComputer.run [8] [|3;9;8;9;10;9;4;9;99;-1;8|]
-|> List.head |> solution "05b.1b" 1
+// Tests
 
-intcodeComputer.run [7] [|3;9;7;9;10;9;4;9;99;-1;8|]
-|> List.head |> solution "05b.2a" 1
-intcodeComputer.run [8] [|3;9;7;9;10;9;4;9;99;-1;8|]
-|> List.head |> solution "05b.2b" 0
+// intcodeComputer.run [7] [|3;9;8;9;10;9;4;9;99;-1;8|]
+// |> List.head |> solution "05b.1a" 0
+// intcodeComputer.run [8] [|3;9;8;9;10;9;4;9;99;-1;8|]
+// |> List.head |> solution "05b.1b" 1
 
-intcodeComputer.run [7] [|3;3;1108;-1;8;3;4;3;99|]
-|> List.head |> solution "05b.3a" 0
-intcodeComputer.run [8] [|3;3;1108;-1;8;3;4;3;99|]
-|> List.head |> solution "05b.3b" 1
+// intcodeComputer.run [7] [|3;9;7;9;10;9;4;9;99;-1;8|]
+// |> List.head |> solution "05b.2a" 1
+// intcodeComputer.run [8] [|3;9;7;9;10;9;4;9;99;-1;8|]
+// |> List.head |> solution "05b.2b" 0
 
-intcodeComputer.run [7] [|3;3;1107;-1;8;3;4;3;99|]
-|> List.head |> solution "05b.4a" 1
-intcodeComputer.run [8] [|3;3;1107;-1;8;3;4;3;99|]
-|> List.head |> solution "05b.4b" 0
+// intcodeComputer.run [7] [|3;3;1108;-1;8;3;4;3;99|]
+// |> List.head |> solution "05b.3a" 0
+// intcodeComputer.run [8] [|3;3;1108;-1;8;3;4;3;99|]
+// |> List.head |> solution "05b.3b" 1
 
-intcodeComputer.run [0] [|3;12;6;12;15;1;13;14;13;4;13;99;-1;0;1;9|]
-|> List.head |> solution "05b.5a" 0
-intcodeComputer.run [1] [|3;12;6;12;15;1;13;14;13;4;13;99;-1;0;1;9|]
-|> List.head |> solution "05b.5b" 1
+// intcodeComputer.run [7] [|3;3;1107;-1;8;3;4;3;99|]
+// |> List.head |> solution "05b.4a" 1
+// intcodeComputer.run [8] [|3;3;1107;-1;8;3;4;3;99|]
+// |> List.head |> solution "05b.4b" 0
 
-intcodeComputer.run [0] [|3;3;1105;-1;9;1101;0;0;12;4;12;99;1|]
-|> List.head |> solution "05b.6a" 0
-intcodeComputer.run [1] [|3;3;1105;-1;9;1101;0;0;12;4;12;99;1|]
-|> List.head |> solution "05b.6b" 1
+// intcodeComputer.run [0] [|3;12;6;12;15;1;13;14;13;4;13;99;-1;0;1;9|]
+// |> List.head |> solution "05b.5a" 0
+// intcodeComputer.run [1] [|3;12;6;12;15;1;13;14;13;4;13;99;-1;0;1;9|]
+// |> List.head |> solution "05b.5b" 1
 
-intcodeComputer.run [7] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
-|> List.head |> solution "05b.7a" 999
-intcodeComputer.run [8] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
-|> List.head |> solution "05b.7b" 1000
-intcodeComputer.run [9] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
-|> List.head |> solution "05b.7c" 1001
+// intcodeComputer.run [0] [|3;3;1105;-1;9;1101;0;0;12;4;12;99;1|]
+// |> List.head |> solution "05b.6a" 0
+// intcodeComputer.run [1] [|3;3;1105;-1;9;1101;0;0;12;4;12;99;1|]
+// |> List.head |> solution "05b.6b" 1
+
+// intcodeComputer.run [7] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
+// |> List.head |> solution "05b.7a" 999
+// intcodeComputer.run [8] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
+// |> List.head |> solution "05b.7b" 1000
+// intcodeComputer.run [9] [|3;21;1008;21;8;20;1005;20;22;107;8;21;20;1006;20;31;1106;0;36;98;0;0;1002;21;125;20;4;20;1105;1;46;104;999;1105;1;46;1101;1000;1;20;4;20;1105;1;46;98;99|]
+// |> List.head |> solution "05b.7c" 1001
 
--- 2019/Run.fsx
+++ 2019/Run.fsx
@@ -1,4 +1,10 @@
+#load "utils/Base.fsx"
+open Base
+
 #load "Day01.fsx"
 #load "Day02.fsx"
 #load "Day03.fsx"
 #load "Day04.fsx"
+#load "Day05.fsx"
+
+solution "All" 0 0

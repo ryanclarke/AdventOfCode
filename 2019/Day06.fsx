4ff375da14360453f7e79af8dd502b735ad61578
Ryan Clarke <ryan@ryanclarke.net>
2019-12-07 07:26:03 -0500

    2019.06b Solved

--- 2019/Day06.fsx
+++ 2019/Day06.fsx
@@ -22,10 +22,31 @@ let solve (orbits:seq<string[]>) =
     |> Seq.map (fun (r,c) -> r,(getCenter c 1))
     |> Seq.sumBy snd
 
-
-
 inputFile "06test1" (ssplit ")")
 |> solve |> solution "06a.1" 42
 
 inputFile "06" (ssplit ")")
 |> solve |> solution "06a" 147223
+
+let solveB (orbits:seq<string[]>) =
+    let tups = orbits |> Seq.map toTuples
+    let map = tups |> Map.ofSeq
+
+    let rec getCenter path =
+        match path with
+        | "COM"::_ -> path |> Set.ofList
+        | head::_ -> getCenter (map.[head]::path)
+        | err -> failwithf "Invalid path: %A" err
+
+    let you,san =
+        map
+        |> Map.map (fun _ c -> getCenter [c])
+        |> fun m -> m.["YOU"],m.["SAN"]
+    
+    (Set.difference you san |> Set.count) + (Set.difference san you |> Set.count)
+
+inputFile "06test2" (ssplit ")")
+|> solveB |> solution "06a.1" 4
+
+inputFile "06" (ssplit ")")
+|> solveB |> solution "06b" 340

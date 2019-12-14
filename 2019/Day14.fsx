c2fe4cd197a48d5e181374c8e9d96a86082a061b
Ryan Clarke <ryan@ryanclarke.net>
2019-12-14 13:52:26 -0500

    Improve 2019.14 input handling

--- 2019/Day14.fsx
+++ 2019/Day14.fsx
@@ -1,53 +1,6 @@
 #load "utils/Base.fsx"
 open Base
 
-let example1 ="157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
-                |> ssplit "\n"
-
-let example2 ="2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
-17 NVRVD, 3 JNWZP => 8 VPVL
-53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
-22 VJHF, 37 MNCFX => 5 FWMGM
-139 ORE => 4 NVRVD
-144 ORE => 7 JNWZP
-5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
-5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
-145 ORE => 6 MNCFX
-1 NVRVD => 8 CXFTF
-1 VJHF, 6 MNCFX => 4 RFSQX
-176 ORE => 6 VJHF"
-                |> ssplit "\n"
-
-let example3 ="171 ORE => 8 CNZTR
-7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
-114 ORE => 4 BHXH
-14 VRPVC => 6 BMBT
-6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-5 BMBT => 4 WPTQ
-189 ORE => 9 KTJDG
-1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
-12 VRPVC, 27 CNZTR => 2 XDBXC
-15 KTJDG, 12 BHXH => 5 XCVML
-3 BHXH, 2 VRPVC => 7 MZWV
-121 ORE => 7 VRPVC
-7 XCVML => 6 RJRHP
-5 BHXH, 4 VRPVC => 5 LTCX"
-                |> ssplit "\n"
-
-let puzzle =
-    inputFile "14" string
-
 type Batch = {
     Chem: string
     Need: int64
@@ -73,9 +26,9 @@ let parse line =
 // 2019.14 A
 // ===================================================================================================================
 
-let solveA description fuelCount =
+let solveA input fuelCount =
     let reactions =
-        description
+        inputFile input string
         |> Seq.map parse
         |> Seq.toList
         |> List.append ["ORE",({Chem = "ORE"; Need = 1L},[])] 
@@ -122,36 +75,36 @@ let solveA description fuelCount =
     |> fun x -> x.["ORE"]
 
 
-solveA example1 1L
+solveA "14test1" 1L
 |> solution "14a.1" 13312L
-solveA example2 1L
+solveA "14test2" 1L
 |> solution "14a.2" 180697L
-solveA example3 1L
+solveA "14test3" 1L
 |> solution "14a.3" 2210736L
-solveA puzzle 1L
+solveA "14" 1L
 |> solution "14a" 136771L
 
 // ===================================================================================================================
-// 2019.12 B
+// 2019.14 B
 // ===================================================================================================================
 
 
-let solveB description =
+let solveB input =
     let mutable change = 1_000_000_000_000L
     let mutable fuel = 0L
     while change > 0L do
         let guessFuel = fuel + change
-        let ore = solveA description guessFuel
+        let ore = solveA input guessFuel
         if ore > 1_000_000_000_000L
         then change <- change / 10L
         else fuel <- guessFuel
     fuel
 
-solveB example1
+solveB "14test1"
 |> solution "14b.1" 82892753L
-solveB example2
+solveB "14test2"
 |> solution "14b.2" 5586022L
-solveB example3
+solveB "14test3"
 |> solution "14b.3" 460664L
-solveB puzzle
+solveB "14"
 |> solution "14b" 8193614L

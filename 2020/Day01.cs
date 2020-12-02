3439036317452b8d0189c3553ef370ea94d4458a
Ryan Clarke <ryan@ryanclarke.net>
2020-12-02 06:33:46 -0500

    2020 Day01

---
+++ 2020/Day01.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+
+namespace AoC2020
+{
+    [Command(name: "day01")]
+    public class Day01 : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            var expenses = File
+                .ReadAllLines("input/01.txt")
+                .Select(int.Parse)
+                .ToList();
+
+            var output = expenses
+                .SelectMany((a, idxA) => expenses
+                    .Skip(idxA)
+                    .Select((b, idxB) => (a + b, a * b)))
+                .First(x => x.Item1 == 2020)
+                .Item2;
+            console.Output.WriteLine(output);
+            
+            output = expenses
+                .SelectMany((a, idxA) => expenses
+                    .Skip(idxA)
+                    .SelectMany((b, idxB) => expenses
+                        .Skip(idxB)
+                        .Select((c, idxC) => (a + b + c, a * b * c))))
+                .First(x => x.Item1 == 2020)
+                .Item2;
+            console.Output.WriteLine(output);
+
+            return default;
+        }
+    }
+}
---
+++ 2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+
+namespace AoC2020
+{
+    public static class Program
+    {
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+    }
+}

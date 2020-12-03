1a7b5c3a8a4dae43d918871a71d65636b305d708
Ryan Clarke <ryan@ryanclarke.net>
2020-12-03 07:04:19 -0500

    2020 Day03

---
+++ 2020/Day03.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+
+namespace AoC2020
+{
+    [Command(name: "day03")]
+    public class Day03 : ICommand
+    {
+        private static readonly List<string> Input = File
+            .ReadAllLines("input/03.txt")
+            .ToList();
+
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            var trees = new[]
+            {
+                (1, 1),
+                (1, 3),
+                (1, 5),
+                (1, 7),
+                (2, 1)
+            }
+                .Select(TreesForSlope)
+                .ToList();
+            
+            console.Output.WriteLine(trees[1]);
+            console.Output.WriteLine(trees.Aggregate((a, t) => a * t));
+
+            return default;
+        }
+
+        private static long TreesForSlope((int down, int right) slope) =>
+            Input
+                .Where((_, i) => i % slope.down == 0)
+                .Select(x => x.ToCharArray())
+                .Select((x, i) => x[(i * slope.right) % x.Length])
+                .Count(x => x == '#');
+    }
+}

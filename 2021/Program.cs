37d693ce45dae97b0f8f31abc8cc0b752d589f96
Ryan Clarke <ryan@ryanclarke.net>
2021-12-17 14:37:46 -0500

    AoC 2021 quality of life upgrades

--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,19 +1,30 @@
 ﻿global using System.Diagnostics;
 global using AoC2021;
 
-//Day01.Run();
-//Day02.Run();
-//Day03.Run();
-//Day04.Run();
-//Day05.Run();
-//Day06.Run();
-//Day07.Run();
-//Day08.Run();
-//Day09.Run();
-//Day10.Run();
-//Day11.Run();
-//Day12.Run();
-//Day13.Run();
-//Day14.Run();
-//Day15.Run();
-Day16.Run();
+var days = System.Reflection.Assembly
+    .GetExecutingAssembly()
+    .GetTypes()
+    .Where(t => t.Name.StartsWith("Day") && t.Name != "Day00");
+
+if (args.Count() == 0)
+{
+    RunDay(days.MaxBy(t => t.Name));
+}
+else
+{
+    Environment.CurrentDirectory = "C:/dev/AdventofCode/2021/bin/Debug/net6.0/";
+
+    if (int.TryParse(args[0], out int dayNumber))
+    {
+        RunDay(days.First(t => t.Name == $"Day{dayNumber:00}"));
+    }
+    foreach (var day in days)
+    {
+        RunDay(day);
+    }
+}
+
+static void RunDay(Type? day)
+{
+    day?.GetMethod("Run")?.Invoke(day, null);
+}

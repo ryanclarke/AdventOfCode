154c876957aa2fa8dbfb3faee327be2d6b79e62e
Ryan Clarke <ryan@ryanclarke.net>
2021-12-17 07:29:41 -0500

    AoC 2021 Day17

---
+++ 2021/Day17.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC2021;
+
+public class Day17
+{
+    public static void Run()
+    {
+        var targetX = (start: 248, end: 285);
+        var targetY = (start: -85, end: -56);
+        //targetX = (start: 20, end: 30);
+        //targetY = (start: -10, end: -5);
+
+        Enumerable.Range(0, Math.Abs(targetY.start)).Sum().Dump("17a (3570): ");
+
+        var velocities = new HashSet<(int,int)>();
+        for (int y = targetY.start; y <= Math.Abs(targetY.start); y++)
+        {
+            for (int x = 0; x <= targetX.end; x++)
+            {
+                var ys = Enumerable.Range(0, Math.Abs(targetY.start) * 2 + 1)
+                    .Aggregate(new List<int>(), 
+                            (l, i) => l.Append(l.LastOrDefault() + y - i).ToList())
+                    .ToList();
+                var xs = Enumerable.Range(0, Math.Abs(targetY.start) * 2 + 1)
+                    .Aggregate(new List<int>(),
+                            (l, i) => l.Append(l.LastOrDefault() + Math.Max(0, x - i)).ToList())
+                    .ToList();
+                if (xs.Zip(ys).Any(p => p.First >= targetX.start && p.First <= targetX.end && p.Second >= targetY.start && p.Second <= targetY.end))
+                {
+                    velocities.Add((x, y));
+                }
+            }
+        }
+        velocities.Count.Dump("17b (): ");
+    }
+}

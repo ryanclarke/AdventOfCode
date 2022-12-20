349e0704ce7820ddce93b123f23e5ec6d28ea0cc
Ryan Clarke <ryan@ryanclarke.net>
2022-12-20 06:01:08 -0500

    AoC 2022 Day 16 & 19 WIP

---
+++ 2022/Day16.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+namespace AoC2022;
+
+public class Day16
+{
+    private static readonly string InputFilePath = $"{Config.InputRoot}/16.txt";
+    public static void Run()
+    {
+        var input = File
+                .ReadAllLines(InputFilePath)
+                .ToList();
+
+        var valves = input
+            .Select(line =>
+            {
+                var (name, connects) = Regex
+                        .Matches(line, @"\b[A-Z]{2}\b")
+                        .Select(m => m.Value)
+                        .ToArray() switch
+                        {
+                            [var first, .. var rest] => (first, rest),
+                            _ => throw new Exception()
+                        };
+                var flowRate = int.Parse(Regex.Match(line, @"[\d]+").Value);
+                return new Valve(name, flowRate, connects);
+            })
+            .ToDictionary(v => v.Name, ID);
+
+        var minute = 0;
+        var currentName = "AA";
+
+        Visit(currentName).Dump();
+    }
+
+    private static int Visit(string Name)
+    {
+        return 0;
+    }
+
+    private record Valve(string Name, int FlowRate, string[] Connects);
+}
---
+++ 2022/Day19.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2022;
+
+public partial class Day19
+{
+    private static readonly string InputFilePath = $"{Config.InputRoot}/19i.txt";
+    public static void Run()
+    {
+        var input = File
+                .ReadAllLines(InputFilePath)
+                .ToList();
+
+        var blueprints = input
+            .Select(line => IntRegex().Matches(line).Select(m => m.Value).Select(int.Parse).ToArray())
+            .Select(m => new Blueprint(m[0], new List<Cost>
+            {
+                new Cost(Robot.Ore, new Inventory(m[1], 0, 0, 0)),
+                new Cost(Robot.Clay, new Inventory(m[2], 0, 0, 0)),
+                new Cost(Robot.Obsidian, new Inventory(m[3], m[4], 0, 0)),
+                new Cost(Robot.Geode, new Inventory(m[5], 0, m[6], 0)),
+            }))
+            .ToList();
+
+
+        var z = 12;
+    }
+
+    private record Blueprint(int Id, List<Cost> Costs);
+    private record Cost(Robot Robot, Inventory Inventory);
+    private record Inventory(int Ore, int Clay, int Obsidian, int Geode);
+    private enum Robot
+    {
+        Ore,
+        Clay,
+        Obsidian,
+        Geode
+    }
+
+    [GeneratedRegex("\\d+")]
+    private static partial Regex IntRegex();
+}

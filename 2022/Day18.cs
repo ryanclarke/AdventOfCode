a46af9768ccc3aaecaad647533d9bf2ac1a86cf7
Ryan Clarke <ryan@ryanclarke.net>
2022-12-20 05:56:16 -0500

    AoC 2022 Day 18a

---
+++ 2022/Day18.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC2022;
+
+public class Day00
+{
+    private static readonly string InputFilePath = $"{Config.InputRoot}/00.txt";
+    public static void Run()
+    {
+        var input = File
+                .ReadAllLines(InputFilePath)
+                .ToList();
+    }
+}

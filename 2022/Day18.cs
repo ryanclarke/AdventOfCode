d657b3750d84b582ece05845ee7162039b0edf93
Ryan Clarke <ryan@ryanclarke.net>
2022-12-20 05:57:32 -0500

    AoC 2022 Day 18a, include code

--- 2022/Day18.cs
+++ 2022/Day18.cs
@@ -1,12 +1,38 @@
-﻿namespace AoC2022;
+﻿using System.Numerics;
 
-public class Day00
+namespace AoC2022;
+
+public class Day18
 {
-    private static readonly string InputFilePath = $"{Config.InputRoot}/00.txt";
+    private static readonly string InputFilePath = $"{Config.InputRoot}/18.txt";
     public static void Run()
     {
         var input = File
                 .ReadAllLines(InputFilePath)
                 .ToList();
+
+        var points = input
+            .Select(line => line
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray()
+                .X(a => new Vector3(a[0], a[1], a[2])))
+            .ToList();
+
+        var neighbors = new []
+        {
+            Vector3.UnitX * 1,
+            Vector3.UnitX * -1,
+            Vector3.UnitY * 1,
+            Vector3.UnitY * -1,
+            Vector3.UnitZ * 1,
+            Vector3.UnitZ * -1,
+        };
+
+        points
+            .Sum(point => neighbors
+                .Count(neighbor => !points.Contains(point + neighbor)))
+            .Dump("18a (3326): ");
+
     }
 }
